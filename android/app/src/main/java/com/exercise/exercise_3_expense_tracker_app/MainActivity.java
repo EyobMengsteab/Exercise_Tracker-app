61c1b5a8c3827532459acf5023be8762d9f8ca55
EyobMengsteab <eyobmengsteab7@gmail.com>
2026-09-18 18:07:29 +0200

    Initialized flutter structure and start the Exercise Tracker App

---
+++ android/app/src/main/java/com/exercise/exercise_3_expense_tracker_app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.exercise.exercise_3_expense_tracker_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'package:exercise_3_expense_tracker_app/expenses.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+        home: Expenses(),
+    ),
+  );
+}
+
